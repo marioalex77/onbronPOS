b376cc4ec79e688f7d60b99d68020a050fdd02e9
Mario Guzman <marioalex77@hotmail.com>
2017-04-26 16:59:02 -0600

    update 26/04/2017 16:58

---
+++ src/main/java/com/maguzman/onbron/beans/Categoria.java
@@ -0,0 +1,67 @@
+package com.maguzman.onbron.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private int idCategoria;
+    @Column(name="nombre", nullable=false)
+    private String nombre;
+
+    public Categoria() {
+        this.nombre = "";
+    }
+
+    public Categoria(String nombre){
+
+        this.nombre = nombre;
+    }
+
+    public Categoria(int idCategoria, String nombre){
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
