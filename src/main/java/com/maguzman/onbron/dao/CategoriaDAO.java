fa051bb113b89cf4fd8c1ba13b6e2771a4cc0f6a
Mario Guzman <marioalex77@hotmail.com>
2017-04-27 12:13:03 -0600

    update 27/04/2017 12:09 Se agregan beans, categorias, configuraciones y servicios

--- src/main/java/com/maguzman/onbron/dao/CategoriaDAO.java
+++ src/main/java/com/maguzman/onbron/dao/CategoriaDAO.java
@@ -8,9 +8,9 @@ import com.maguzman.onbron.beans.Categoria;
 import java.util.List;
 
 public interface CategoriaDAO {
-    public abstract Categoria buscarPorClave(Integer id);
-    public abstract List<Categoria> buscarTodos();
-    public abstract void salvar(Categoria categoria);
-    public abstract void borrar(Integer id);
-    public abstract Categoria actualizar(Categoria categoria);
+    Categoria buscarPorClave(Integer id);
+    List<Categoria> buscarTodos();
+    void salvar(Categoria categoria);
+    void borrar(Integer id);
+    Categoria actualizar(Categoria categoria);
 }
